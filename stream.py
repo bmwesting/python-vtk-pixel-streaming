9b17c286626450d25ac0284a4db63a36e52666bf
Brandt Westing <bmwesting@gmail.com>
2013-01-29 16:12:34 -0600

    Now compressing w/ JPG

--- stream.py
+++ stream.py
@@ -10,7 +10,7 @@ renwin = vtkRenderWindow()
 renwin.AddRenderer(ren) 
 renwin.SetInteractor(iren)
 
-# create a jpg writer to write pixel data to jpg in memory
+# The WindowToImageFilter grabs the framebuffer pixels
 w2i = vtk.vtkWindowToImageFilter()
 w2i.SetInput(renwin)
 
@@ -25,13 +25,22 @@ ren.AddActor(actor)
 def keypress(obj, event):
 	if obj.GetKeySym() == 's':
 		print 'Sending pixel stream.'
-        w2i.Update()
         renwin.Render()
+        w2i.Update()
         imageData = w2i.GetOutput()
-        raw = VN.vtk_to_numpy(imageData.GetPointData().GetScalars())
-        array = numpy.array(raw.flatten(), numpy.uint8)
-        img = Image.fromstring('RGB',(300,300), array, 'raw', 'RGB')
-        img.save('my.png')
+
+        # Convert the vtk 2d framebuffer into a numpy array
+        rawImage = VN.vtk_to_numpy(imageData.GetPointData().GetScalars())
+        rawImageArray = numpy.array(rawImage.flatten(), numpy.uint8)
+
+        # convert to a PIL image so we can compress as JPG and stream over a socket
+        img = Image.fromstring('RGB',(300,300), rawImageArray, 'raw', 'RGB')
+
+        # StringIO servers as a memory endpoint for the compressed JPEG
+        output = StringIO()
+        img.save(output, format="JPEG")
+        #img.show()
+
 
 iren.AddObserver("KeyPressEvent", keypress)
 
